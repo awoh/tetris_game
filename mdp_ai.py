c0cf647afbbda46203c6aed2013691ff24d54c25
Sarah Eisenach <sarah@eisenach.org>
2019-04-16 00:54:37 -0400

    fixed everything

--- mdp_ai.py
+++ mdp_ai.py
@@ -50,15 +50,16 @@ class TetrisAI(object):
             elif board.currentX < next_move[1]:
                 board.moveRight()
             k += 1
-        # lines = BOARD_DATA.dropDown()
-        lines = board.moveDown()    # creates new piece after moving down
+
+        lines = board.dropDown()    # creates new piece after moving down
         return lines, board
 
-    def getRandomState(num_moves):
+    def getRandomState():
         """
         Creates initial random state for training
         """
         # let regular tetris_ai calculateScore get random initial moves for state in D
+        num_moves = random.randint(5,15)    # number of moves to make when creating init state
         board = BoardData()
         board.createNewPiece()  # create initial piece
         for i in range(num_moves):
@@ -68,7 +69,7 @@ class TetrisAI(object):
         return board
 
 
-    def rollout(board, steps, curr_val, init_action):
+    def rollout(board, steps, curr_val, curr_policy, init_action):
         """
         Generate rollout for a given state (goes steps number of actions into future)
         Returns the total reward for the rollout
@@ -88,24 +89,52 @@ class TetrisAI(object):
             if i == 0:
                 next_move = init_action
             else:
-                next_move = curr_policy.predict(curr_state)
+                next_move = getAction(curr_policy.predict((curr_state.getData, curr_state.currentShape)))
             curr_state = makeMove(next_move, curr_state)    # makes move, and then creates next piece
 
         # compute the unbiased estimate (v), prev_v based on m moves away from s
-        tot_reward += exp(gamma, steps) * curr_val.predict(curr_state)
+        tot_reward += exp(gamma, steps) * curr_val.predict((curr_state.getData, curr_state.currentShape))
 
         return tot_reward
 
-    def mpi():
+    def getActionSet(board):
+        """
+        Determines A based on the board state, A = {(rotation, column)}
+        max size of A = 34 (for L, J, and T)
+        we want size A = 40 (try every possible rotation/column pair)
+        """
+        A = list()
+        for r in range(4):
+            for c in range(10):
+                if(board.tryMoveCurrent(r, c, 0)):
+                    A.append((r,c)) # add to set
+                else:
+                    A.append((-1,-1))
+        return A
+
+    def getAction():
+        """
+        return action for policy, chooses max from classifier output
+        """
+        pass
+
+    def play(policy):
+        board = BoardData()
+        game_lines = 0
+        while board.createNewPiece():
+            next_move = getAction(policy.predict((board.getData, board.currentShape)))
+            lines, board = makeMove(next_move, board)
+            game_lines += lines
+
+        return game_lines
+
+    def mpi(A, N, M, m, init_policy, init_val):
         """
         starts with init policy and value and generates a sequence of value-new_policy
         pairs (v_k = evaluation step and policy_k+1 = greedy step)
         """
-        curr_policy     # policy_1 (arbitrary)
-        curr_val        # value_0 function (arbitrary)
-        A = {}      #action set (32 possible actions)...what are these???
-        M = 1
         done = False
+        curr_policy_score = 0
 
         # i don't know what k is... maybe just do while not done (until new_policy = curr_policy)
         while !done:
@@ -116,30 +145,30 @@ class TetrisAI(object):
             val_set = np.array([])
             q_set = np.array([])
 
-            num_moves = random.randint(5,15)    # number of moves to make when creating init state
-
             # sampling N states from distribution (each iteration, generating new random state)
             for i in range(N):
 
                 # generate random initial state by making num_moves number of moves
-                s = getRandomState(num_moves)
+                s = getRandomState()
 
                 # generate rollout for state of size m (go up to m steps away)
-                v_hat = rollout(s, m, curr_val, curr_val.predict(s))
+                action = getAction(curr_policy.predict((s.getData, s.currentShape)))
+                v_hat = rollout(s, m, curr_val, curr_policy, action)
 
                 # add (s, v) to training set for regressor (x = state_set, y = val_set)
-                state_set = np.append(state_set, [s])
+                state_set = np.append(state_set, [(s.getData, s.currentShape)])
                 val_set = np.append(val_set, [v_hat])
 
                 state_q = np.array([])
 
                 # for every possible action from state s, make action and then follow policy for m steps
+                A = getActionSet(s)
                 for a in A:
                     # build M rollouts  (get rewards for all future states (1 -> m+1))
                     tot_Q = 0
                     for i in range(M):
                         # build rollout set (size m+1) from this state (going further in future), i.e. [(s, a, r)...]
-                        R = rollout(s, m+1, curr_val, a)
+                        R = rollout(s, m+1, curr_val, curr_policy, a)
                         tot_Q += R
 
                     # calculate Q_hat
@@ -159,18 +188,36 @@ class TetrisAI(object):
             # take min of the output set and get action
             new_policy = svm.LinearSVC(max_iter = 1000)
             new_policy.fit(state_set, q_set)
-            curr_policy = new_policy      # update current policy function
 
+            # evaluate policy by averaging score over 20 games
+            tot_lines = 0
+            for i in range(20):
+                num_lines = play(new_policy)
+                tot_lines += num_lines
+
+            new_policy_score = tot_lines/20
 
             # if curr_policy is approximately new_policy, save policy into file end
-            if(curr_policy == new_policy):
+            policy_change = new_policy_score - curr_policy_score
+            if(policy_change < error_threshold and policy_change >=0):
                 # return curr_policy
                 # save the model to disk, CAN USE PICKLE OR JOBLIB
-                pickle.dump(model, open('finalized_policy.sav', 'wb'))
+                pickle.dump(new_policy, open('finalized_policy.sav', 'wb'))
                 break
-                
 
-# some time later...
+            # update current policy function and score
+            curr_policy = new_policy
+            curr_policy_score = new_policy_score
+
 def main:
+
+    init_policy    # policy_1 (arbitrary)
+    init_val      # value_0 function (arbitrary)
+    A = {}      #action set (32 possible actions)...what are these???
+    M = 1
+    N = 100
+    m = 5
+    mpi(A, N, M, m, init_policy, init_val)
+
     # load the model from disk
     policy = pickle.load(open('finalized_policy.sav', 'rb'))
