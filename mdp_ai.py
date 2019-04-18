437b12f7bc679f320fcc59323bca237f650ed6fe
Sarah Eisenach <sarah@eisenach.org>
2019-04-18 15:38:33 -0400

    fixed bugs - need to add actual rewards

--- mdp_ai.py
+++ mdp_ai.py
@@ -27,7 +27,7 @@ import random
 # evaluation function to evaluate all states:
     # f is defined as linear combo of set of features, phi
     # f(.) = phi * (.) * theta, theta = parameter vector (policy/controller)
-def nextMove(weights, board):
+def nextInitialMove(weights, board):
     """
     Return the next move for the board.
     Used for generating the initial states
@@ -39,7 +39,7 @@ def nextMove(weights, board):
     for a in action_set:
         # do action
         score =0
-        if a == (-1,-1):
+        if a == (-1,-1, 0):
             score = -sys.maxsize -1
         else:
             lines, new_board = makeMove(a, board)
@@ -50,6 +50,13 @@ def nextMove(weights, board):
     best = best.flatten().tolist()
     max = np.random.choice(best)
     return action_set[max]
+
+def calculateReward(weights, features):
+    tot_score = 0
+    for i in range(len(weights)):
+        tot_score += weights[i]*features[i]
+    return tot_score
+
 
 def makeMove(next_move, board):
     """
@@ -81,14 +88,14 @@ def getRandomState():
     board.createNewPiece()  # create initial piece
     for i in range(num_moves):
         policy = [1]*7
-        next_move = nextMove(policy, board)
+        next_move = nextInitialMove(policy, board)
         lines, board = makeMove(next_move, board)
 
     return board
 
 def getFeatures(s):
-    feature_array = np.array([1]*35)
-    return feature_array
+    feature_array = [1]*35
+    return np.array(feature_array)
 
 def getActionSet(board):
     """
@@ -100,95 +107,94 @@ def getActionSet(board):
     for r in range(4):
         for c in range(10):
             if(board.tryMoveCurrent(r, c, 0)):
-                A.append((r,c)) # add to set
+                A.append((r,c,0)) # add to set
             else:
-                A.append((-1,-1))
+                A.append((-1,-1,0))
     return A
 
 def getAction(board, policy, action_set):
     """
     return action for policy, chooses max from classifier output
     """
-    board_features = getFeatures(board)
-    piece = board.currentShape.shape
-    # one hot encode piece
-    piece = [0]*7
-    piece[board.currentShape.shape -1] = 1
-    # need policy_weights = one array of 35 + 7
-    actions = policy.predict([board_features, piece])
-    best = np.argwhere(actions == np.amax(actions))
-    best = best.flatten().tolist()
-    max = np.random.choice(best)
-    return action_set[max]
+    # if policy doesn't exist yet, choose action randomly, else get from policy model
+    if policy == None:
+        possible_actions = [i for i in action_set if i[0] > -1]
+        rand_i = random.randint(0, len(possible_actions)-1)
+        action = possible_actions[rand_i]
+        print(action)
+    else:
+        piece = [0]*7   # one hot encode piece
+        piece[board.currentShape.shape -1] = 1
+        tot_features = np.append(getFeatures(board), [piece])
+        action_scores = policy.predict([tot_features])
+        best_scores = np.argwhere(action_scores == np.amax(action_scores)).flatten().tolist()
+        max_score = np.random.choice(best_scores)
+        action =  action_set[max_score]
+    return action
 
-def calculateReward(weights, features):
-    tot_score = 0
-    for i in range(len(weights)):
-        tot_score += weights[i]*features[i]
-    return tot_score
-
-def rollout(board, steps, curr_val, curr_policy, init_action, action_set):
+def rollout(curr_state, steps, curr_val, curr_policy, init_action, action_set, gamma):
     """
     Generate rollout for a given state (goes steps number of actions into future)
     Returns the total reward for the rollout
     """
-    curr_state = s
-    s_features = getFeatures(curr_state)
     tot_reward = 0
     curr_reward = 0 # PROBABLY SHOULD CHANGE???????????
+
     # need to go from 0 to m-1, m is an upper bound (since game may end earlier)
     for i in range(steps):
-        # get state, action, reward tuple
-
-        # WHEN DO YOU UPDATE ACTION?? (AFTER REWARD OR BEFORE?),
-        # IS INITIAL STATE INCLUDED IN REWARD??
-        # curr_reward = calculateReward(curr_state)   # curr_reward is the immediate reward (score, ie # lines cleared)
-        tot_reward += exp(gamma,i) * curr_reward    # add gamma*reward to sum of rewards
-
-        # make next move... if first iteration, then use provided action (handles for R(s,a) calculations)
+        # get action... if first iteration, then use provided action (handles for R(s,a) calculations)
         if i == 0:
             next_move = init_action
         else:
             next_move = getAction(curr_state, curr_policy, action_set)
-        curr_reward, curr_state = makeMove(next_move, curr_state)    # makes move, and then creates next piece
-        s_features = getFeatures(curr_state)
 
-    # compute the unbiased estimate (v), prev_v based on m moves away from s
-    tot_reward += exp(gamma, steps) * calculateReward(curr_val.coefs_, s_features)
-    # curr_val.predict(s_features)
+        curr_reward, curr_state = makeMove(next_move, curr_state)    # make move, cur_reward = lines cleared by action
 
+        # curr_reward = calculateReward(curr_state), curr_reward is the immediate reward (score, ie # lines cleared)
+        tot_reward += (gamma**i) * curr_reward    # add gamma*reward to sum of rewards
+
+    # if haven't developed a model for value function yet...idk
+    if(curr_val == None):
+        v = 0
+    else:
+        s_features = getFeatures(curr_state)    # get state, so can predict using model
+        v = curr_val.predict(s_features)
+
+    tot_reward += (gamma**steps) * v # compute the unbiased estimate (v), prev_v based on m moves away from s
     return tot_reward
 
 def play(policy):
     board = BoardData()
     game_lines = 0
     while board.createNewPiece():
-        next_move = getAction(policy.predict((board.getData, board.currentShape)))
+        A = getActionSet(board)
+        next_move = getAction(board, policy, A)
         lines, board = makeMove(next_move, board)
         game_lines += lines
 
     return game_lines
 
-def mpi(N, M, m, error_threshold):
+def mpi(N, M, m, error_threshold, gamma):
     """
     starts with init policy and value and generates a sequence of value-new_policy
     pairs (v_k = evaluation step and policy_k+1 = greedy step)
     """
     done = False
     curr_policy_score = 0
-    print("hi")
     # i don't know what k is... maybe just do while not done (until new_policy = curr_policy)
     while not done:
-        print("hi")
     # for k  in range(idk):
         # at every iteration, build new value function and policy function
+        num_features = 35
+        val_features = np.empty((0,num_features))
+        val_outputs = np.empty((0,1))
 
-        state_set = np.array([])
-        val_set = np.array([])
-        q_set = np.array([])
+        tot_features = num_features + 7
+        policy_features = np.empty((0, tot_features))
+        policy_outputs = np.empty((0,40)) # output values: max of q_set - q_hat for given action
 
-        curr_val = [1]*35
-        curr_policy = [1]*40  # weights are all the same, so would pick random action
+        curr_val = None
+        curr_policy = None  # weights are all the same, so would pick random action
 
         # sampling N states from distribution (each iteration, generating new random state)
         for i in range(N):
@@ -200,54 +206,56 @@ def mpi(N, M, m, error_threshold):
 
             # generate rollout for state of size m (go up to m steps away)
             action = getAction(s, curr_policy, A)
-            v_hat = rollout(s, m, curr_val, curr_policy, action, A)
-
+            v_hat = rollout(s, m, curr_val, curr_policy, action, A, gamma)
 
             # add (s, v) to training set for regressor (x = state_set, y = val_set)
             # s is represented by the features determined in board
-            state_set = np.append(state_set, [s_features])
-            val_set = np.append(val_set, [v_hat])
+            val_features = np.append(val_features, np.array([s_features]), axis=0)
+            val_outputs = np.append(val_outputs, np.array([[v_hat]]), axis=0)
 
             state_q = np.array([])
 
             # for every possible action from state s, make action and then follow policy for m steps
-
             for a in A:
-                # build M rollouts  (get rewards for all future states (1 -> m+1))
                 tot_Q = 0
 
                 # if action not possible...
-                if a == (-1,-1):
+                if a == (-1,-1,0):
                     pass
+
+                # build M rollouts  (get rewards for all future states (1 -> m+1))
                 for i in range(M):
                     # build rollout set (size m+1) from this state (going further in future), i.e. [(s, a, r)...]
-                    R = rollout(s, m+1, curr_val, curr_policy, a, A)
+                    R = rollout(s, m+1, curr_val, curr_policy, a, A, gamma)
                     tot_Q += R
 
-                # calculate Q_hat
-                Q_hat = tot_Q / M
+                Q_hat = tot_Q / M   # calculate Q_hat
                 state_q = np.append(state_q, [Q_hat])   # add Q_hat value to list of Q's for given state
-
-
-            q_set = np.append(q_set, [state_q])     # output values for classifier (list of Q values for each state)
-                # output values: max of q_set - q_hat for given action
 
             # piece is one-hot encoded
             piece = [0]*7
             piece[s.currentShape.shape -1] = 1
-            classifier_states = np.append(classifier_states, [[s_features, piece]])
+            tot_features = np.append(s_features, [piece])
+            print("tot_features: ")
+            print(tot_features)
 
+            policy_features = np.append(policy_features, np.array([tot_features]), axis=0)
+            policy_outputs = np.append(policy_outputs, np.array([state_q]), axis=0)     # output values for classifier (list of Q values for each state)
+            print("policy outputs: ")
+            print(policy_outputs)
         # learn v_k w/ regressor
         new_val = linear_model.LinearRegression()
-        new_val.fit(state_set, val_set)
+        print("value func data: ")
+        print(val_features)
+        print(val_outputs)
+        new_val.fit(val_features, val_outputs)
         curr_val = new_val      # update current value function
         print(curr_val.coef_)   # prints the weights in the model
 
         # learn new policy w/ classifier
         # input: state, output: set of q_hats (all values for actions of a given set)
-        # take min of the output set and get action
         new_policy = linear_model.LinearRegression()
-        new_policy.fit(classifier_states, q_set)
+        new_policy.fit(policy_features, policy_outputs)
 
         # evaluate policy by averaging score over 20 games
         tot_lines = 0
@@ -256,6 +264,7 @@ def mpi(N, M, m, error_threshold):
             tot_lines += num_lines
 
         new_policy_score = tot_lines/20
+        print("policy score: " + str(new_policy_score))
 
         # if curr_policy is approximately new_policy, save policy into file end
         policy_change = new_policy_score - curr_policy_score
@@ -273,10 +282,11 @@ def main():
 
     # A = {}      #action set (32 possible actions)...what are these???
     M = 1
-    N = 1
+    N = 100
     m = 5
+    gamma = 1
     error_threshold = 0.01
-    mpi(N, M, m, error_threshold)
+    mpi(N, M, m, error_threshold, gamma)
 
     # load the model from disk
     policy = pickle.load(open('finalized_policy.sav', 'rb'))
